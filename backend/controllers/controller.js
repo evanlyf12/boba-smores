e04fde7ae27afb400fbc9f3c3abf55c2cce949c4
Jayce Birrell <66990367+Jayce0808@users.noreply.github.com>
2021-09-29 14:12:59 +1000

    Reverted error-handling changes

--- backend/controllers/controller.js
+++ backend/controllers/controller.js
@@ -69,53 +69,42 @@ const loggedIn = async (req, res) => {
 }
 
 const addContact = async (req, res) => {
-    try {
-        console.log("Got here");
-        const user = await User.findById(req.params.id).orFail();
+    console.log("Got here");
+    const user = await User.findById(req.params.id).orFail();
 
-        const newContact = new Contact({
-            isFavourite: false,
-            contactInformation:
-            {
-                name: { firstName: req.body.firstname, lastName: req.body.lastname },
-                company: { name: req.body.company, isVisible: true },
-                location: { city: req.body.city, country: req.body.country, isVisible: true },
-                phone: { number: req.body.phone, isVisible: true },
-                email: { address: req.body.email, isVisible: true },
-                socials: {
-                    facebook: req.body.facebook,
-                    instagram: req.body.instagram,
-                    linkedin: req.body.linkedin,
-                    isVisible: true
-                },
-                lastCatchup: { date: req.body.date, isVisible: true },
-                commonInterests: { tags: [], isVisible: true },
-                tags: { tags: [], isVisible: true },
-                notes: { notes: req.body.notes, isVisible: true }
-            }
-        });
+    const newContact = new Contact({
+        isFavourite: false,
+        contactInformation:
+        {
+            name: { firstName: req.body.firstname, lastName: req.body.lastname },
+            company: { name: req.body.company, isVisible: true },
+            location: { city: req.body.city, country: req.body.country, isVisible: true },
+            phone: { number: req.body.phone, isVisible: true },
+            email: { address: req.body.email, isVisible: true },
+            socials: {
+                facebook: req.body.facebook,
+                instagram: req.body.instagram,
+                linkedin: req.body.linkedin,
+                isVisible: true
+            },
+            lastCatchup: { date: req.body.date, isVisible: true },
+            commonInterests: { tags: [], isVisible: true },
+            tags: { tags: [], isVisible: true },
+            notes: { notes: req.body.notes, isVisible: true }
+        }
+    });
 
-        await newContact.save(function (err) {
-            if (err)
-            {
-                res.sendStatus(400)
-            }
-        });
+    await newContact.save();
 
-        console.log(newContact._id);
+    console.log(newContact._id);
 
-        user.contacts.push(newContact._id);
+    user.contacts.push(newContact._id);
 
-        user.markModified("contacts");
+    user.markModified("contacts");
 
-        await user.save();
+    await user.save();
 
-        res.sendStatus(201);
-
-    }
-    catch (error) {
-        res.sendStatus(404);
-    }
+    res.sendStatus(200);
 
 }
 
@@ -148,87 +137,72 @@ const deleteContact = async (req, res) => {
 
 const updateContact = async (req, res) => {
 
-    // console.log("IN CONTACT");
-    // console.log(req.body)
-    try {
-        const contact = await Contact.findById(req.params.id).orFail();
-        console.log(contact);
-        // update all fields with passed data from front-end (including unchanged ones)
-        contact.isFavourite = req.body.isFavourite;
-        contact.contactInformation.name.firstName = req.body.contactInformation.name.firstName;
-        contact.contactInformation.name.lastName = req.body.contactInformation.name.lastName;
-        contact.contactInformation.company.name = req.body.contactInformation.company.name;
-        contact.contactInformation.location.city = req.body.contactInformation.location.city;
-        contact.contactInformation.location.country = req.body.contactInformation.location.country;
-        contact.contactInformation.phone.number = req.body.contactInformation.phone.number;
-        contact.contactInformation.email.address = req.body.contactInformation.email.address;
-        contact.contactInformation.socials.facebook = req.body.contactInformation.socials.facebook;
-        contact.contactInformation.socials.instagram = req.body.contactInformation.socials.instagram;
-        contact.contactInformation.socials.linkedin = req.body.contactInformation.socials.linkedin;
-        contact.contactInformation.lastCatchup.date = req.body.contactInformation.lastCatchup.date;
+    console.log("IN CONTACT");
+    console.log(req.body)
+    const contact = await Contact.findById(req.params.id);
+    console.log(contact);
+    // update all fields with passed data from front-end (including unchanged ones)
+    contact.contactInformation.name.firstName = req.body.contactInformation.name.firstName;
+    contact.contactInformation.name.lastName = req.body.contactInformation.name.lastName;
+    contact.contactInformation.company.name = req.body.contactInformation.company.name;
+    contact.contactInformation.location.city = req.body.contactInformation.location.city;
+    contact.contactInformation.location.country = req.body.contactInformation.location.country;
+    contact.contactInformation.phone.number = req.body.contactInformation.phone.number;
+    contact.contactInformation.email.address = req.body.contactInformation.email.address;
+    contact.contactInformation.socials.facebook = req.body.contactInformation.socials.facebook;
+    contact.contactInformation.socials.instagram = req.body.contactInformation.socials.instagram;
+    contact.contactInformation.socials.linkedin = req.body.contactInformation.socials.linkedin;
+    contact.contactInformation.lastCatchup.date = req.body.contactInformation.lastCatchup.date;
 
-        // Qu: How will arrays be sent to backend?
-        contact.contactInformation.commonInterests.tags = req.body.contactInformation.commonInterests.tags
-        // Note: need to markModified for arrays. e.g.
-        contact.contactInformation.commonInterests.markModified("tags");
+    // Qu: How will arrays be sent to backend?
+    contact.contactInformation.commonInterests.tags = req.body.contactInformation.commonInterests.tags
+    // Note: need to markModified for arrays. e.g.
+    contact.contactInformation.commonInterests.markModified("tags");
 
-        contact.contactInformation.tags.tags = req.body.contactInformation.tags.tags
-        contact.contactInformation.tags.markModified("tags");
+    contact.contactInformation.tags.tags = req.body.contactInformation.tags.tags
+    contact.contactInformation.tags.markModified("tags");
 
-        contact.contactInformation.notes.notes = req.body.contactInformation.notes.notes
-        contact.contactInformation.notes.markModified("notes");
+    contact.contactInformation.notes.notes = req.body.contactInformation.notes.notes
+    contact.contactInformation.notes.markModified("notes");
 
-        // save changes
-        await contact.save();
+    // save changes
+    await contact.save();
 
-        res.sendStatus(200);
-    }
-    catch (error) {
-        res.sendStatus(400);
-    }
+    res.sendStatus(201);
 
 }
 
 // to retrieve contact from backend and send to front-end 
 const getContacts = async (req, res) => {
-    try {
-        let contacts = [];
-        // console.log(req.params)
-        const user = await User.findById(req.params.id).orFail();
-        // console.log(user);
-        for (var i = 0; i < user.contacts.length; i++) {
-            var contact = await Contact.findById(user.contacts[i]).lean();
-            contacts[i] = contact;
-        }
-        // receive desired contact from front-end (by ID)
-        // send contact details to front-end
-        res.status(200);
-        res.send(contacts);
+    let contacts = [];
+    // console.log(req.params)
+    const user = await User.findById(req.params.id).orFail();
+    // console.log(user);
+    for (var i = 0; i < user.contacts.length; i++) {
+        var contact = await Contact.findById(user.contacts[i]).lean();
+        contacts[i] = contact;
     }
-    catch (error) {
-        res.sendStatus(400);
-    }
+    // receive desired contact from front-end (by ID)
+    // send contact details to front-end
+    res.status(200);
+    res.send(contacts);
+    // receive desired contact from front-end (by ID)
+    // send contact details to front-end
+    res.send(contacts);
 
 }
 
 // add (existing) tag to a contact 
 const addTagToContact = async (req, res) => {
 
-    try {
-        const newTag = new Tag({
-            text: req.body.text,
-            colour: req.body.colour
-        })
-        // tag need to be added to contact
+    const newTag = new Tag({
+        text: req.body.text,
+        colour: req.body.colour
+    })
+    // tag need to be added to contact
 
-        // and added to database (if not exist already?)
-        await newTag.save();
-
-        res.sendStatus(200);
-    }
-    catch (error) {
-        res.sendStatus(400);
-    }
+    // and added to database (if not exist already?)
+    await newTag.save();
 }
 
 
