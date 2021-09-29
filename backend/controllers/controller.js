60caec5ce28eefc7e83d0f08f99cc4b8f8247419
Jayce Birrell <66990367+Jayce0808@users.noreply.github.com>
2021-09-29 12:15:26 +1000

    Added try and catch to rest of the backend functions

--- backend/controllers/controller.js
+++ backend/controllers/controller.js
@@ -14,36 +14,42 @@ const test = async (req, res) => {
 
 // Verify the Google provided ID token is valid
 const authenticateUser = async (req, res) => {
-    const { token } = req.body.data;
-    //substring(1, (req.body.data.length - 1));
-    const ticket = await client.verifyIdToken({
-        idToken: token,
-        audience: process.env.CLIENT_ID
-    });
-    const { firstName, lastName, email, picture } = ticket.getPayload();
+    try{
+        const { token } = req.body.data;
+        //substring(1, (req.body.data.length - 1));
+        const ticket = await client.verifyIdToken({
+            idToken: token,
+            audience: process.env.CLIENT_ID
+        });
+        const { firstName, lastName, email, picture } = ticket.getPayload();
 
-    // update or create a user in the CRM database
-    const user = await User.findOneAndUpdate(
-        {email: email},
-        {firstName: firstName, lastName: lastName, picture: picture }
-    )
+        // update or create a user in the CRM database
+        const user = await User.findOneAndUpdate(
+            {email: email},
+            {firstName: firstName, lastName: lastName, picture: picture }
+        )
 
-    if(!user)
-    {
-        const user = new User({
-            email: email,
-            firstname: firstName,
-            lastname: lastName,
-            picture: picture
-        })
-        await user.save();
+        if(!user)
+        {
+            const user = new User({
+                email: email,
+                firstname: firstName,
+                lastname: lastName,
+                picture: picture
+            })
+            await user.save();
+        }
+        req.session.userId = user.id;
+
+        // user = JSON.parse(user);
+
+        res.status(201)
+        res.json(user)
     }
-    req.session.userId = user.id;
-
-    // user = JSON.parse(user);
-
-    res.status(201)
-    res.json(user)
+    catch(error)
+    {
+        res.sendStatus(400);
+    }
 }
 
 const createAccount = async (req, res) => {
@@ -65,63 +71,77 @@ const loggedIn = async (req, res) => {
 }
 
 const addContact = async (req, res) => {
-    console.log("Got here");
-    const user = await User.findById(req.params.id);
+    try{
+        console.log("Got here");
+        const user = await User.findById(req.params.id);
 
-    const newContact = new Contact({
-        isFavourite: false,
-        contactInformation:
-        {
-            name: { firstName: req.body.firstname, lastName: req.body.lastname },
-            company: { name: req.body.company, isVisible: true },
-            location: { city: req.body.city, country: req.body.country, isVisible: true },
-            phone: { number: req.body.phone, isVisible: true },
-            email: { address: req.body.email, isVisible: true },
-            socials: {
-                facebook: req.body.facebook,
-                instagram: req.body.instagram,
-                linkedin: req.body.linkedin,
-                isVisible: true
-            },
-            lastCatchup: { date: req.body.date, isVisible: true },
-            commonInterests: { tags: [], isVisible: true },
-            tags: { tags: [], isVisible: true },
-            notes: { notes: req.body.notes, isVisible: true }
-        }
-    });
+        const newContact = new Contact({
+            isFavourite: false,
+            contactInformation:
+            {
+                name: { firstName: req.body.firstname, lastName: req.body.lastname },
+                company: { name: req.body.company, isVisible: true },
+                location: { city: req.body.city, country: req.body.country, isVisible: true },
+                phone: { number: req.body.phone, isVisible: true },
+                email: { address: req.body.email, isVisible: true },
+                socials: {
+                    facebook: req.body.facebook,
+                    instagram: req.body.instagram,
+                    linkedin: req.body.linkedin,
+                    isVisible: true
+                },
+                lastCatchup: { date: req.body.date, isVisible: true },
+                commonInterests: { tags: [], isVisible: true },
+                tags: { tags: [], isVisible: true },
+                notes: { notes: req.body.notes, isVisible: true }
+            }
+        });
 
-    await newContact.save();
+        await newContact.save();
 
-    console.log(newContact._id);
+        console.log(newContact._id);
 
-    user.contacts.push(newContact._id);
+        user.contacts.push(newContact._id);
 
-    user.markModified("contacts");
+        user.markModified("contacts");
 
-    await user.save();
+        await user.save();
 
-    res.sendStatus(201);
+        res.sendStatus(201);
+
+    }
+    catch(error)
+    {
+        res.sendStatus(400);
+    }
 
 }
 
 const deleteContact = async (req, res) => {
-    const contactId = req.params.contactId;
-    const userId = req.params.userId;
+    try
+    {
+        const contactId = req.params.contactId;
+        const userId = req.params.userId;
 
-    let user = await User.findById(userId);
+        let user = await User.findById(userId).orFail();
 
-    const index = user.contacts.indexOf(contactId);
+        const index = user.contacts.indexOf(contactId);
 
-    if (index > -1) user.contacts.splice(index, 1);
+        if (index > -1) user.contacts.splice(index, 1);
 
-    user.markModified("contacts");
+        user.markModified("contacts");
 
-    await user.save();
+        await user.save();
 
-    Contact.findByIdAndDelete(contactId, function (err) {
-        if (err) return handleError(err);
-        res.sendStatus(200);
-    });
+        Contact.findByIdAndDelete(contactId, function (err) {
+            if (err) return handleError(err);
+            res.sendStatus(200);
+        });
+    }
+    catch(error)
+    {
+        res.sendStatus(400);
+    }
 
 
 }
@@ -172,36 +192,48 @@ const updateContact = async (req, res) => {
 
 // to retrieve contact from backend and send to front-end 
 const getContacts = async (req, res) => {
-    // console.log("IN GET CONTACTS");
-    let contacts = [];
-    // console.log(req.params)
-    const user = await User.findById(req.params.id);
-    // console.log(user);
-    for (var i = 0; i < user.contacts.length; i++)
+    try
     {
-        var contact = await Contact.findById(user.contacts[i]).lean();
-        contacts[i] = contact;
+        let contacts = [];
+        // console.log(req.params)
+        const user = await User.findById(req.params.id).orFail();
+        // console.log(user);
+        for (var i = 0; i < user.contacts.length; i++)
+        {
+            var contact = await Contact.findById(user.contacts[i]).lean();
+            contacts[i] = contact;
+        }
+        // receive desired contact from front-end (by ID)
+        // send contact details to front-end
+        res.status(200);
+        res.send(contacts);
     }
-    // receive desired contact from front-end (by ID)
-    // send contact details to front-end
-    res.status(200);
-    res.send(contacts);
+    catch(error)
+    {
+        res.sendStatus(400);
+    }
 
 }
 
 // add (existing) tag to a contact 
 const addTagToContact = async (req, res) => {
 
-    const newTag = new Tag({
-        text: req.body.text,
-        colour: req.body.colour
-    })
-    // tag need to be added to contact
+    try{
+        const newTag = new Tag({
+            text: req.body.text,
+            colour: req.body.colour
+        })
+        // tag need to be added to contact
 
-    // and added to database (if not exist already?)
-    await newTag.save();
+        // and added to database (if not exist already?)
+        await newTag.save();
 
-    res.sendStatus(200);
+        res.sendStatus(200);
+    }
+    catch(error)
+    {
+        res.sendStatus(400);
+    }
 }
 
 
