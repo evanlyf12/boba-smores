b173f8fd1623c2ea78d8f35a2e0c35282e006a3c
alicetai <alice.tai@student.unimelb.edu.au>
2021-10-07 18:34:46 +1100

    made obj data update correctly

--- web/src/components/FilterDropdown.jsx
+++ web/src/components/FilterDropdown.jsx
@@ -2,29 +2,55 @@ import React, {useState, useEffect} from 'react';
 import { Icon } from '@iconify/react';
 
 function FilterDropdown(props) {
-
-    const [isOpen, setIsOpen] = useState(false);
-    const toggleDropdown = async (event) => {
-        event.preventDefault();
-        setIsOpen(!isOpen);
-    }
-
     const contacts = props.value;
 
     // make an array of countries for the filter to display
     const getCountries = () => {
-        let countries =[];
+        let countries = {};
         // get the unique country names from user's contacts
         contacts.forEach(contact => {
-            countries.push(contact.contactInformation.location.country);
+            countries[contact.contactInformation.location.country] = false;
         });
-        // return unique countries only, and sorted
-        return [... new Set(countries.sort())]
+        // return unique countries only, and sorted (should be automatic)
+        return countries;
     }
+
+    const countries = getCountries();
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleDropdown = async (event) => {
+        event.preventDefault();
+        setIsOpen(!isOpen);
+
+        if (!isOpen) {
+            clearFilter();
+        }
+    }
+
+    function clearFilter() {
+        // reset state to initial values (all false)
+        setChecked(getCountries());
+    }
+    
+    const [checked, setChecked] = useState(countries);
+    
+    // event handler for checkboxes
+    const handleCheck = (event) => {
+
+        // update the country object value
+        setChecked({
+            ...checked,
+            [event.target.name]: event.target.checked,
+        });
+    };
+    // update the entire countries object
+    Object.assign(countries, checked)
+    console.log(countries);
+
 
     return (
         <div className="filter box">
-            <form>
+            <form id="countries-filter">
                 <button className="dropdown-button" onClick={toggleDropdown}>
                     Filter by country
                     <Icon icon="bx:bx-caret-down" width="15" height="15" />
@@ -32,15 +58,13 @@ function FilterDropdown(props) {
 
                 {isOpen && (<div className={`dropdown ${isOpen ? 'open' : 'closed'}`}>
                     <ul>
-                        {getCountries().map(country => (
-                        <li>
-                            <input type="checkbox" id="au" name="australia" value="Australia"/>
-                            <label for="au">{country}</label>
+                        {Object.keys(countries).map((country, selected) => (
+                        <li> 
+                            <input type="checkbox" id={country} name={country} value={country} onChange={handleCheck}/>
+                            <label for={country}>{country}</label>
                         </li>
                         ))}
-
                     </ul>
-
                 </div>)}
             </form>
         </div>
