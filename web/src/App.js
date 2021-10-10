661e0ca4e9da71992f562169ae08ebf098eaa9a9
alicetai <58418239+alicetai@users.noreply.github.com>
2021-10-11 02:45:43 +1100

    fixed url problem displaying contacts info (i think)

--- web/src/App.js
+++ web/src/App.js
@@ -13,12 +13,7 @@ function App() {
         <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,600;0,700;1,800&display=swap" />
         <Router>
             <Switch>
-            <Route path='/dashboard' render={() => 
-                        <>
-                        <Dashboard  />
-                        </>
-                        }
-                    />
+            <Route path='/dashboard' exact component={Dashboard}/>
             <Route path="/login" exact component={Login}/>  
             <Route path="/*" exact component={Login}/>  
             </Switch>
