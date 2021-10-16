d721dad35477de4b2d188625a580342351f95297
Jayce Birrell <66990367+Jayce0808@users.noreply.github.com>
2021-10-16 17:32:29 +1100

    Added favourite API

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -34,4 +34,6 @@ router.get('/get_com_interests/:id', controller.getComInterests)
 
 router.get('/get_tags_from_contact/:contactId', controller.getTagsFromContact)
 
+router.post('/set_favourite/:contactId', controller.setFavourite)
+
 module.exports = router
